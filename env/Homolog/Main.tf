9290d0d9cf0166c38517a674ff3025698607f71b
Alexandre Silva <abatistas1709@gmail.com>
2026-01-04 09:57:10 -0300

    Altera imagem a ser usada

--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -4,7 +4,7 @@ module "homolog" {
     nome_repositorio = "homolog"
     cargoIAM = "homolog"
     ambiante = "homolog"
-    imagem="leonardosartorello/go_ci:22"
+    imagem="abatistas/cursoci2:latest"
 }
 
 output "IP_alb" {
